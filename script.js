cccd5a3ae8eb1f5f10a6dd2fecbc264fc0d4fcbd
rein-007 <182231134+rein-007@users.noreply.github.com>
2024-10-16 16:31:11 +0800

    adjustment

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btn = document.querySelectorAll('button');
 // const special = document.querySelectorAll('#special');
 let btnClick = '';
 let btnClass = '';
+let btnText = '';
 let resultText = document.querySelector('.result-text');
 let num1 = 0;
 let num2 = 0;
@@ -16,72 +17,66 @@ btn.forEach((button) => {
     button.addEventListener('click', () => {
         btnClick = button.id;
         btnClass = button.className;
+        btnText = button.textContent;
         console.log(btnClick);
         if (btnClick==='operate') {
-            resultText.textContent = result;
             if (operators==true) {
+                resultText.textContent = result;
                 btnClick = 'special';
                 btnClass = 'equals';
-            }
-            operators = true;
-            operator = button.className;
-        } else if (btnClick==='num') {
-            console.log(button.textContent);
-            if (resultText.textContent==0) {
-                result = Number(button.textContent);
+                specialBtn();
             } else {
-                result = Number(resultText.textContent.concat(button.textContent));
+                operators = true;
+                operator = btnClass;
             }
-            (operators==true) ? num2 = Number(resultText.textContent) : num1 = Number(resultText.textContent);
+        } else if (btnClick==='num') {
+            // console.log(btnText);
+            if (resultText.textContent==0) {
+                resultText.textContent = btnText;
+            } else {
+                resultText.textContent = resultText.textContent.concat(btnText);
+            }
+            (operators==true || equal==true) ? num2 = Number(resultText.textContent) : num1 = Number(resultText.textContent);
             if (equal==true) {
-                resultText.textContent = 0;
+                resultText.textContent = btnText;
             }
+            // result = resultText.textContent;
             equal = false;
+            operators = false;
+            console.log(resultText.textContent);
+            console.log(typeof(resultText.textContent));
         } else if (btnClick==='special') {
-            if (button.className==='clear') {
-                resultText.textContent = 0;
-                num1 = 0;
-                num2 = 0;
-                result = 0;
-                operators = false;
-                equal = false;
-            } else if (btnClass==='equals') {
-                if (operators==true) {
-                    // const calc = new operate();
-                    operate(num1, operator, num2);
-                    resultText.textContent = result;
-                    console.log(operator);
-                    console.log(typeof(num1));
-                    console.log(typeof(num2));
-                    console.log(resultText.textContent);
-                    console.log(typeof(resultText.textContent));
-                    operators = false;
-                    equal = true;
-                    num1 = Number(resultText.textContent);
-                }
-            }
+            specialBtn();
         }
     });
 });
 
+function specialBtn () {
+    if (btnClass==='clear') {
+        resultText.textContent = 0;
+        num1 = 0;
+        num2 = 0;
+        result = 0;
+        operators = false;
+        equal = false;
+    } else if (btnClass==='equals') {
+        if (operators==true) {
+            // const calc = new operate();
+            operate(num1, operator, num2);
+            resultText.textContent = result;
+            console.log(operator);
+            console.log(typeof(num1));
+            console.log(typeof(num2));
+            console.log(resultText.textContent);
+            console.log(typeof(resultText.textContent));
+            operators = false;
+            equal = true;
+            num1 = Number(result);
+        }
+    }
+}
+
 function operate(num1, operator, num2) {
-    // if (operator==='add') {
-    // const add = function (num1, num2) {
-        // return num1 + num2;
-    // };
-    // }
-
-    // const subtract = function (num1, num2) {
-    //     return num1 - num2;
-    // };
-
-    // const multiply = function (array) {
-    //     return num1 * num2;
-    // };
-
-    // const divide = function (array) {
-    //     return num1 / num2;
-    // };
     switch (operator) {
         case 'add':
             result = num1 + num2;
